c0ef383e343cf44e352d48ef537e26b6cbd04d31
andre <61792917+luuxis@users.noreply.github.com>
2022-06-04 13:25:29 +0200

    test

---
+++ build/main/index.js
@@ -0,0 +1,16 @@
+var jovanta = ["default", "ready", "createWindow", "push", "shift", "0x0", "0x1", "0x2", "electron", "0x3", "0x4", "electron-debug", "app", "0x5", "0x6", "0x7", "0x8", "0x9", "0xa", "info", "0xb", "0xc", "web-contents-created", "0xd", "0xf", "0x10", "window-all-closed", "0x11", "0x12", "0x13", "setWindowOpenHandler", "shell", "openExternal", "deny", "platform", "__esModule", "./windows/UpdateWindow", "requestSingleInstanceLock", "quit", "whenReady", "then"];
+
+(function(annalysse, tyeishia) {
+    var koralee = guenevere => {
+        while (--guenevere) {
+            annalysse.push(annalysse.shift());
+        }
+    };
+    koralee(++tyeishia);
+}(jovanta, 317));
+
+var athanasius = (jeydi, malulani) => {
+    jeydi = jeydi - 0;
+    var karinda = jovanta[jeydi];
+    return karinda;
+};
